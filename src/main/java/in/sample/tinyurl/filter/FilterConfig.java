88572d74fae62f187369ecc845d416e053f98b5f
Naveen Jagirdhar <48186392+Naveen-Jagirdhar@users.noreply.github.com>
2025-04-04 17:21:01 +0530

    add rate limiting

---
+++ src/main/java/in/sample/tinyurl/filter/FilterConfig.java
@@ -0,0 +1,18 @@
+package in.sample.tinyurl.filter;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public FilterRegistrationBean<RateLimitFilter> rateLimiterFilter() {
+        FilterRegistrationBean<RateLimitFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RateLimitFilter());
+        registrationBean.addUrlPatterns("/api/v1/short-url/*");
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
+}
+
